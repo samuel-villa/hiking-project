4a0fa31b16b6273c37560583f8da7626087ffb7f
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-05-27 12:19:11 +0200

    no change

--- hiking-project/routes/web.php
+++ hiking-project/routes/web.php
@@ -7,19 +7,7 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     //return view('welcome');
 
-    $hike = new \App\Models\Hike();
-    $hike->name = 'Cascade de Chanxhe';
-    $hike->distance = '11.38km';
-    $hike->duration = '3h';
-    $hike->elevation_gain = '318m';
-    $hike->description = "Très belle promenade au départ de Chanxhe passant par Fays, La Haze, Lincé et les cascades de Chanxhe.
-Le parcours est très varié : bois, prairies, petites routes, chemins, sentiers.
-Le point fort de la promenade ce sont les cascades, il y en a 2 à moins de 200m l'une de l'autre.
-Attention : récemment on a barré le chemin d'accès aux cascades avec un câble et un panneau propriété privée mais vous pouvez sauter par dessus sans problème.";
-    $hike->user_id = 1;
-    $hike->save();
 
-    return $hike;
 });
 
 # !!! Test connection to DB OK !!!
