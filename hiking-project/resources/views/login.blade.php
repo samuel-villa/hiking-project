79e1669087441484a345e252a75f0cedb63b0881
samuel-villa <ciullasamuel@gmail.com>
2024-05-27 11:34:10 +0200

    [IMP] login form

--- hiking-project/resources/views/login.blade.php
+++ hiking-project/resources/views/login.blade.php
@@ -1,1 +1,17 @@
-Login Form
+
+@csrf
+<form action="" method="GET">
+    <h1>Login</h1>
+    <div>
+        <label for="email">Email:</label>
+        <input type="text" name="email">
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="text" name="password">
+    </div>
+    <div>
+        <button>Login</button>
+    </div>
+</form>
+    
