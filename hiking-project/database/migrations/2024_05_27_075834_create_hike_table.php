4a2d9a5fd4fb5a56f84aa571447c079680107032
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-05-27 10:19:46 +0200

    Database delete + Hikes table reconstruction

---
+++ hiking-project/database/migrations/2024_05_27_075834_create_hike_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hikes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('distance');
+            $table->string('duration');
+            $table->string('elevation_gain');
+            $table->longText('description');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hike');
+    }
+};
