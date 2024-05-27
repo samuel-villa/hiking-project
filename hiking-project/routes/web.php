dfa299a226fd3e594c096081bf15b00786a97934
samuel-villa <ciullasamuel@gmail.com>
2024-05-27 12:13:01 +0200

    [ADD] installed breeze for authentication

--- hiking-project/routes/web.php
+++ hiking-project/routes/web.php
@@ -1,59 +1,27 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    //return view('welcome');
-
-    $hike = new \App\Models\Hike();
-    $hike->name = 'Cascade de Chanxhe';
-    $hike->distance = '11.38km';
-    $hike->duration = '3h';
-    $hike->elevation_gain = '318m';
-    $hike->description = "Très belle promenade au départ de Chanxhe passant par Fays, La Haze, Lincé et les cascades de Chanxhe.
-Le parcours est très varié : bois, prairies, petites routes, chemins, sentiers.
-Le point fort de la promenade ce sont les cascades, il y en a 2 à moins de 200m l'une de l'autre.
-Attention : récemment on a barré le chemin d'accès aux cascades avec un câble et un panneau propriété privée mais vous pouvez sauter par dessus sans problème.";
-    $hike->user_id = 1;
-    $hike->save();
-
-    return $hike;
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-# !!! Test connection to DB OK !!!
-// Route::get('/test-db', function () {
-//     $data = DB::table('Users')->get();
-//     return dump($data);
-// });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/subscribe', function () {
-    return 'subscribe';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login', [UserController::class, 'login'])->name('login');
-
-Route::get('/profile', function () {
-    return 'profile';
-});
-
-Route::get('/profile/hikes', function () {
-    return 'profile/{profile_name}/hikes';
-});
-
-Route::get('/profile/starred', function () {
-    return 'profile/{profile_name}/starred';
-});
-
-Route::get('/hike', function () {
-    return 'hike-{hike-slug}';
-});
-
-Route::get('/edit-hike', function () {
-    return 'hike-{hike-slug}/edit';
-});
-
-Route::get('/delete-hike', function () {
-    return 'hike-{hike-slug}/delete';
-});
+require __DIR__.'/auth.php';
