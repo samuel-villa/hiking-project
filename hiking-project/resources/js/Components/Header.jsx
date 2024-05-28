64cb648f8f4544f993b924cd092d2e1a9605eaef
samuel-villa <ciullasamuel@gmail.com>
2024-05-28 09:44:41 +0200

    [IMP] menu

--- hiking-project/resources/js/Components/Header.jsx
+++ hiking-project/resources/js/Components/Header.jsx
@@ -18,20 +18,12 @@ export default function Header({ auth }) {
                             </div>
                             <nav className="-mx-3 flex flex-1 justify-end">
                                 {auth.user ? (
-                                    <>
-                                        <Link
+                                    <Link
                                         href={route('dashboard')}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
-                                        Hikes
+                                        Dashboard
                                     </Link>
-                                        <Link
-                                            href={route('dashboard')}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Dashboard
-                                        </Link>
-                                    </>
                                 ) : (
                                     <>
                                         <Link
