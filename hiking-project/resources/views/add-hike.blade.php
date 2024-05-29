637fb75003e528770d4af6d98fc28d9768aac55a
samuel-villa <ciullasamuel@gmail.com>
2024-05-29 16:13:28 +0200

    [IMP] add hike page

--- hiking-project/resources/views/add-hike.blade.php
+++ hiking-project/resources/views/add-hike.blade.php
@@ -1,3 +1,4 @@
+@include('partials/header')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <div class="container">
 
@@ -38,7 +39,9 @@
             <input type="file" class="form-control" id="picture" name="picture">
         </div> --}}
 
-        <div class="mb-3"><button type="submit" class="btn btn-primary">Save Hike</button></div>
+        <div class="mb-3"><bu
+            tton type="submit" class="btn btn-primary">Save Hike</bu></div>
 
     </form>
 </div>
+@include('partials/footer')
