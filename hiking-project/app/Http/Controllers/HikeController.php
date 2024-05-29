23184b5709dae749c4b8c561c65abd18fe59d4e2
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-05-29 16:05:59 +0200

    merge

--- hiking-project/app/Http/Controllers/HikeController.php
+++ hiking-project/app/Http/Controllers/HikeController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Hike;
-use App\Models\Picture;
 
 
 class HikeController extends Controller
@@ -51,6 +50,7 @@ class HikeController extends Controller
 
     public function add_picture(Request $request)
     {
+
     }
 
     public function create(Request $request)
@@ -67,31 +67,19 @@ class HikeController extends Controller
 
         $userId = Auth::id();
 
-        // dd($request);
         // dd($request->input('name'));
         // dd($validatedData);
 
-        if ($request->hasFile('picture')) {
-            $picture = $request->file('picture');
-            $path = $picture->store('pictures', 'public'); // Store the picture in the 'public/pictures' directory
+        Hike::create([
+            'name' => $request->input('name'),
+            'distance' => $request->input('distance'),
+            'duration' => $request->input('duration'),
+            'elevation_gain' => $request->input('elevation_gain'),
+            'description' => $request->input('description'),
+            'trail_rank' => '100',
+            'user_id' => $userId,
 
-
-            $hike = Hike::create([
-                'name' => $request->input('name'),
-                'distance' => $request->input('distance'),
-                'duration' => $request->input('duration'),
-                'elevation_gain' => $request->input('elevation_gain'),
-                'description' => $request->input('description'),
-                'trail_rank' => '100',
-                'user_id' => $userId,
-            ]);
-
-            // Create the picture record
-            Picture::create([
-                'hike_id' => $hike->id,
-                'image_path' => $path,
-            ]);
-        }
+        ]);
 
         return redirect()->route('home');
     }
