dfa9e09d144ee9508f2992e2621fd96c2158d8f9
samuel-villa <ciullasamuel@gmail.com>
2024-05-30 09:59:09 +0200

    [IMP] header

--- hiking-project/resources/views/partials/header.blade.php
+++ hiking-project/resources/views/partials/header.blade.php
@@ -27,8 +27,42 @@
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
+
+            <nav className="-mx-3 flex flex-1 justify-end">
+              @if (Auth::id())
+              <div class="dropdown-center">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  {{ (request()->user()->name) }}
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
+                  {{-- <li><a class="dropdown-item" href="{{ route('') }}">My Hikes</a></li> --}}
+                  <li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                       Log Out
+                    </a>
+                </li>
+                
+                </ul>
+              </div>
+              @else
+                      <a href={{route('login')}} className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                          Log in
+                      </a>
+                      <a href={{route('register')}} className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                          Register
+                      </a>
+              @endif
+
+          </nav>
           </div>
         </div>
       </nav>
 
 </header>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
