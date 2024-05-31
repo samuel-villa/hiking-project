a746a4b362a04e2376e1d88394235b4cc05f482b
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-05-31 16:24:33 +0200

    styles

--- hiking-project/resources/views/home.blade.php
+++ hiking-project/resources/views/home.blade.php
@@ -38,7 +38,7 @@
         </div>
     </form> --}}
     @include('partials/tags-dropdown')
-
+<div class="container">
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach($hikes as $hike)
                 <a style="text-decoration:none;" href="{{ route('show-hike', ['id' => $hike->id]) }}">
